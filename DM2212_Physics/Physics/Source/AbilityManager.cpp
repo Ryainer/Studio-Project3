a4990af7bb7891eed165413e76e12e85f696ddd9
klareity <taonanite2014@gmail.com>
2020-08-28 13:03:47 +0800

    DoAbility Update

--- DM2212_Physics/Physics/Source/AbilityManager.cpp
+++ DM2212_Physics/Physics/Source/AbilityManager.cpp
@@ -8,40 +8,41 @@ AbilityManager::~AbilityManager()
 {
 }
 //actor, actee, player
-void AbilityManager::DoAbility(GameObject* go1, GameObject* go2, GameObject* player)
+void AbilityManager::DoAbility(GameObject* go1, GameObject* go2, GameObject* player)//go1 is the projectile, go2 is the target
 {
-	switch (go2->type)//change this to take into account level
+	switch (go1->type)//change this to take into account level
 	{
 	case GameObject::GO_PROJECTILE:
 	{
-		go1->health--;
-		go2->active = false;
-		if (go1->health < 1)
+		go2->health--;
+		go1->active = false;
+		if (go2->health < 1)
 		{
-			go1->active = false;
+			go2->active = false;
+			std::cout << "yeet" << std::endl;
 		}
 		break;
 	}
 	case GameObject::GO_BOOMERANG:
 	{
-		if (go2->range > 0)
+		if (go1->range > 0)
 		{
-			go1->health--;
+			go2->health--;
 			//std::cout << go1->health << std::endl;
 		}
-		if (go2->range <= 0)
+		if (go1->range <= 0)
 		{
-			go1->health -= 2;
+			go2->health -= 2;
 			player->health++;
 			//std::cout << go1->health << std::endl;
 			//std::cout << player->health << std::endl;
 		}
 
-		if (go1->health < 1)
+		if (go2->health < 1)
 		{
-			go1->active = false;
+			go2->active = false;
 		}
-
+		std::cout << "yeetus" << std::endl;
 		break;
 	}
 	}
